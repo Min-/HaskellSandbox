8012d4f74ed332561d490edc9d9b1bb90311c99b
Min- <min_z@outlook.com>
2016-03-03 20:16:14 -0600

    ZeroCrossing

--- src/ZeroCrossing.hs
+++ src/ZeroCrossing.hs
@@ -3,6 +3,8 @@
 
 -- Find index of zero-crossing point
 
+import qualified Safe as Safe
+
 seq1 = [1,1,4,5,7,4,2,0,-1,-3,-9,-5,-2,4,12,15,17,-14,18,1,2,3,4]
 
 zeroCrossing [] = []
@@ -13,6 +15,11 @@ zeroCrossing  xs =
   else 0 : zeroCrossing (drop 1 xs)
     where product2 x = (fst $ head x) * (fst $ head $ drop 1 x)
           ys = zip xs [1..]
-          index = snd . head
-          
-      
+         
+slideFunc f _ _ [] = [] 
+slideFunc f windowSize slideSize xs
+  | slideSize > windowSize = Safe.headNote "Slide size should be smaller than window size" []
+  | length xs < windowSize = []
+  | otherwise = (f $ take windowSize xs) : (slideFunc f windowSize slideSize $ drop slideSize xs)
+
+slideSum = slideFunc sum
