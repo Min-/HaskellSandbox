f6bc669abf71103c6983927c9f1083e00772e1e5
Min- <min_z@outlook.com>
2016-03-04 14:38:27 -0600

    zero crossing real Lamina data

--- src/ZeroCrossing.hs
+++ src/ZeroCrossing.hs
@@ -1,3 +1,14 @@
+{-#LANGUAGE OverloadedStrings#-}
+
+{-
+Project name: Lamina associated domain and find for domain boundaries and gene blocks
+Min Zhang
+Date: March 4, 2016
+Version: v0.1.0
+README: 
+-}
+
+
 -- Min Zhang
 -- March 3, 2016
 
@@ -6,6 +17,45 @@
 import qualified Safe as Safe
 
 seq1 = [1,1,4,5,7,4,2,0,-1,-3,-9,-5,-2,4,12,15,17,-14,18,1,2,3,4]
+
+
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.IO as TextIO
+import qualified Data.Char as C
+import Control.Applicative
+import qualified Data.List as L
+import Control.Monad (fmap)
+import Data.Ord (comparing)
+import Data.Function (on)
+import qualified Safe as S
+import qualified Data.HashMap.Lazy as M
+import qualified Data.Maybe as Maybe
+import qualified Data.Foldable as F (all)
+import Data.Traversable (sequenceA)
+import qualified System.IO as IO
+import System.Environment
+import MyText
+import MyTable
+import Util
+
+inputpath_sample = "data/GSE68260_Clone.5-5.1N.OE_LP150415.head.txt"
+
+-- data structure: matrix with first row as colnames; first 3 columns are: chr start end
+-- data contains NA
+-- need to keep first 3 columns and do transformation functions (e.g. sum, mean etc) for the rest of columns
+
+
+-- matrix transformation
+-- matTrans n1 (number of columns to skip) n2 (number of rows to skip) d (delimiters) f (function for transformation) matrtix
+
+matTrans n1 n2 d f mat = zipWith (\x y -> x ++ [y]) lables (trans mat3)
+  where mat2 = drop n2 $ map (T.splitOn d) $ T.lines mat 
+        mat3 = map (drop n1) mat2
+        lables = map (take n1) mat2
+        trans = map (readDouble . f . map (\x->if x == "NA" then 0 else toDouble x))
+
+mean [] = 0
+mean xs = sum xs / (L.genericLength xs)
 
 zeroCrossing [] = []
 zeroCrossing [x] = []
@@ -23,3 +73,9 @@ slideFunc f windowSize slideSize xs
   | otherwise = (f $ take windowSize xs) : (slideFunc f windowSize slideSize $ drop slideSize xs)
 
 slideSum = slideFunc sum
+
+main = do
+  input <- TextIO.readFile inputpath
+  let res = matTrans 3 1 "\t" (logBase 2 . mean) input
+  return res
+
